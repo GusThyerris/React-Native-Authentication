30ad89eb0b3a808ebec9954182c26bfaa9d93951
gusThyerris <gustavothyerrisdonascimentooli@gmail.com>
2024-03-26 20:55:12 -0300

    update da página "Home

--- pages/pagesApp/Home.js
+++ pages/pagesApp/Home.js
@@ -9,21 +9,65 @@ export default function Home({ navigation }) {
   const [diario, setDiario] = useState([]);
   function deleteDiario(id){
     Firebase.collection("diario").doc(id).delete();
+
+    Alert.alert("O diário foi deletado.");
   }
+
+  useEffect(() => {
+    Firebase.collection("diario").onSnapshot((query) => {
+      const lista = [];
+      query,forEach((doc) => {
+        lista.push({...doc.data(), id: DOMRectList.id});
+      });
+    setDiario(lista);
+    });
+  }, []);
 
   return (
     <View style={styles.container}>
+      <View>
+        <Text style={styles.titulo}> Lista de Dias</Text>
+      </View>
 
-      <Text style={styles.titulo}>Meu diário</Text>
+      <FlatList
+      data={diario}
+      renderItem={({item}) => {
+        return(
+          <View style={styles.musicas}>
 
-      <TextInput
-        style={styles.input}
-        multiline = {true}
-        numberOfLines = {6}
-        placeholder='Querido diário...'
+            <TouchableOpacity onPress={() => navigation.navigate("ChangeApp", {
+              id: item.id,
+              banda: item.artistabanda,
+              genero: item.genero,
+              musica: item.musica
+            })}>
+
+              <View style={styles.itens}>
+
+                <Text style={styles.titulobanda}> Artista/Banda: <Text style={styles.textobanda}> {item.artistabanda} </Text></Text>
+                <Text style={styles.titulobanda}> Genêro: <Text style={styles.textobanda}> {item.genero} </Text></Text>
+                <Text style={styles.titulobanda}> Música: <Text style={styles.textobanda}> {item.musica} </Text></Text>
+              
+              </View>
+
+            </TouchableOpacity>
+
+            <View style={styles.botaodeletar}>
+
+              <TouchableOpacity onPress={() => {deleteMusica(item.id)}}>
+                <MaterialCommunityIcons name="delete-empty" size={70} color="red" />
+              </TouchableOpacity>
+            
+            </View>
+
+          </View>
+        );
+      }}
       />
+      <TouchableOpacity style={styles.addbutton} onPress={() => navigation.navigate("RegisterApp")}>
+        <MaterialCommunityIcons name="plus-circle-outline" size={70} color="green" />
+      </TouchableOpacity>
 
-      <StatusBar style="auto" />
     </View>
   );
 }
@@ -31,17 +75,48 @@ export default function Home({ navigation }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#8B0000',
+    justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: '#8B0000'
   },
   titulo: {
+    marginTop: 50,
     fontSize: 35,
     color: "#ffff"
   },
-  input: {
-    backgroundColor: "#ffff",
-    width: "80%",
-    height: "80%",
+  itens: {
+    marginHorizontal: 10,
+    marginVertical: 10,
+    padding: 10
+  },
+  titulobanda: {
+    fontSize: 13,
+    color: '#fff'
+  },
+  textobanda: {
+    fontFamily: 15,
+    fontWeight: "bold"
+  },
+  musicas: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginHorizontal: 10,
+    marginVertical: 10,
+    padding: 10,
+    backgroundColor: '#0000CD',
     borderRadius: 10
+  },
+  botaodeletar: {
+    textAlignVertical: 'center',
+    marginVertical: 10
+  },
+  addbutton: {
+    backgroundColor: '#ffffff',
+    borderRadius: 50,
+    position: 'absolute',
+    left: 20,
+    bottom: 40,
+    justifyContent: "center",
+    alignItems: "center"
   }
 });
